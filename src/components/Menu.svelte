1480972af030b48e53eb54f2c960d6af4c1f9509
davhyandrade <david.souza95@etec.sp.gov.br>
2023-07-09 14:30:42 -0300

    feat: replace images with SVG

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -1,120 +1,121 @@
+<script>
+	import Clock from './ClockImage.svelte';
+	import Timer from './TimerImage.svelte';
+	import Stopwatch from './StopwatchImage.svelte';
+</script>
+
 <nav>
-    <a href="/">
-        <li>
-            <img src="https://i.postimg.cc/mZz40pwb/image-removebg-preview-2022-10-15-T185502-621.png" alt="relogio">
-            <span>Relógio</span>
-        </li>
-    </a>
-    <a href="/temporizador">
-        <li>
-            <img src="https://i.postimg.cc/HLyK6FmK/temporizador.png" alt="temporizador">
-            <span>Temporizador</span>
-        </li>
-    </a>
-    <a href="/cronometro">
-        <li>
-            <img src="https://i.postimg.cc/vBRT41bK/cronometro.png" alt="cronometro">
-            <span>Cronômetro</span>
-        </li>
-    </a>
+	<a href="/">
+		<li>
+			<Clock />
+			<span>Relógio</span>
+		</li>
+	</a>
+	<a href="/temporizador">
+		<li>
+			<Timer />
+			<span>Temporizador</span>
+		</li>
+	</a>
+	<a href="/cronometro">
+		<li>
+			<Stopwatch/>
+			<span>Cronômetro</span>
+		</li>
+	</a>
 </nav>
 
 <style lang="scss">
-    nav {
-        position: fixed;
-        width: 10%;
-        height: 315px;
-        background: rgba(245, 245, 245, 0.2);
-        border-radius: 15px 0 0 15px;
-        border-right: 1px solid rgb(223, 223, 223);
+	nav {
+		position: fixed;
+		width: 10%;
+		height: 315px;
+		background: rgba(245, 245, 245, 0.2);
+		border-radius: 15px 0 0 15px;
+		border-right: 1px solid rgb(223, 223, 223);
 
-        @media screen and (max-width:800px) {
-            & {
-                width: 100%;
-                height: auto;
-                display: flex;
-                align-items: center;
-                justify-content: space-evenly;
-                position: initial;
-                border-radius: initial;
-                border-right: none;
-                border-bottom: 1px solid rgb(223, 223, 223);
-            }
-        }
+		@media screen and (max-width: 800px) {
+			& {
+				width: 100%;
+				height: auto;
+				display: flex;
+				align-items: center;
+				justify-content: space-evenly;
+				position: initial;
+				border-radius: initial;
+				border-right: none;
+				border-bottom: 1px solid rgb(223, 223, 223);
+			}
+		}
 
-        a {
-            text-decoration: none;
-            list-style-type: none;
-            color: gray;
+		a {
+			text-decoration: none;
+			list-style-type: none;
+			color: gray;
 
-            @media screen and (max-width: 800px) {
-                & {
-                    width: 33%;
-                }
-            }
-            
-            li {
-                height: 105px;
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
-                align-items: center;
-                overflow: hidden;
+			@media screen and (max-width: 800px) {
+				& {
+					width: 33%;
+				}
+			}
 
-                img {
-                    width: 35px;
-                    opacity: 60%;
-                }
+			li {
+				height: 105px;
+				display: flex;
+				flex-direction: column;
+				justify-content: center;
+				align-items: center;
+				overflow: hidden;
 
-                span {
-                    font-size: 0.8rem;
-                    margin-top: 10px;
-                }
+				span {
+					font-size: 0.8rem;
+					margin-top: 10px;
+				}
 
-                &:hover {
-                    transition: 1s;
-                    background: rgba(128, 128, 128, 0.1);
-                }
+				&:hover {
+					transition: 1s;
+					background: rgba(128, 128, 128, 0.1);
+				}
 
-                &:active {
-                    transition: 0.1s;
-                    background: rgba(128, 128, 128, 0.2);
-                }
-            }
+				&:active {
+					transition: 0.1s;
+					background: rgba(128, 128, 128, 0.2);
+				}
+			}
 
-            &:nth-child(1) li {
-                border-radius: 15px 0 0 0;
+			&:nth-child(1) li {
+				border-radius: 15px 0 0 0;
 
-                @media screen and (max-width: 800px) {
-                    & {
-                        border-radius: 5px 0 0 0;
-                    }
-                }
-            }
+				@media screen and (max-width: 800px) {
+					& {
+						border-radius: 5px 0 0 0;
+					}
+				}
+			}
 
-            &:nth-child(2) li {
-                border-top: 1px solid rgb(223, 223, 223);
-                border-bottom: 1px solid rgb(223, 223, 223);
+			&:nth-child(2) li {
+				border-top: 1px solid rgb(223, 223, 223);
+				border-bottom: 1px solid rgb(223, 223, 223);
 
-                @media screen and (max-width: 800px) {
-                    & {
-                        border-right: 1px solid rgb(223, 223, 223);
-                        border-left: 1px solid rgb(223, 223, 223);
-                        border-top: none;
-                        border-bottom: none;
-                    }
-                }
-            }
+				@media screen and (max-width: 800px) {
+					& {
+						border-right: 1px solid rgb(223, 223, 223);
+						border-left: 1px solid rgb(223, 223, 223);
+						border-top: none;
+						border-bottom: none;
+					}
+				}
+			}
 
-            &:nth-child(3) li {
-                border-radius: 0 0 0 15px;
+			&:nth-child(3) li {
+				border-radius: 0 0 0 15px;
 
-                @media screen and (max-width: 800px) {
-                    & {
-                        border-radius: 0 5px 0 0;
-                    }
-                }
-            }
-        }
-    }
+				@media screen and (max-width: 800px) {
+					& {
+						border-radius: 0 5px 0 0;
+					}
+				}
+			}
+		}
+	}
 </style>
